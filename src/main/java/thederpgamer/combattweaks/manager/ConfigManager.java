9862b569e36632aa2bfac9249d0c3c10b87fdeea
Garret Reichenbach <garretreichenbach@gmail.com>
2023-03-25 16:33:03 -0700

    Update ConfigManager.java

--- src/main/java/thederpgamer/combattweaks/manager/ConfigManager.java
+++ src/main/java/thederpgamer/combattweaks/manager/ConfigManager.java
@@ -25,9 +25,9 @@ public class ConfigManager {
 		"repair-paste-power-consumed-per-block-resting: 5",
 		"repair-paste-power-consumed-per-block-charging: 15",
 		"armor-value-multiplier: 10.0",
-		"cannon-armor-multiplier: 0.65",
-		"beam-armor-multiplier: 0.2",
-		"missile-armor-multiplier: 0.5",
+		"cannon-armor-multiplier: 1.0",
+		"beam-armor-multiplier: 0.35",
+		"missile-armor-multiplier: 0.75",
 	};
 
 	public static void initialize(CombatTweaks instance) {
