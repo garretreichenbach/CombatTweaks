e0ce510918334b3fb5e0ed3a4e9b9f3cdde58b0a
Garret Reichenbach <garret2kool@gmail.com>
2023-04-04 18:58:27 -0700

    1.0.5 wip

--- src/main/java/thederpgamer/combattweaks/manager/JumpHandler.java
+++ src/main/java/thederpgamer/combattweaks/manager/JumpHandler.java
@@ -5,10 +5,7 @@ import api.listener.events.entity.ShipJumpEngageEvent;
 import api.network.packets.PacketUtil;
 import org.schema.common.util.linAlg.Vector3i;
 import org.schema.game.common.controller.SegmentController;
-import org.schema.game.common.controller.elements.VoidElementManager;
-import org.schema.game.common.data.blockeffects.config.StatusEffectType;
 import org.schema.game.common.data.player.PlayerState;
-import thederpgamer.combattweaks.network.server.JumpHudRemovePacket;
 import thederpgamer.combattweaks.network.server.JumpHudUpdatePacket;
 
 import java.util.HashMap;
@@ -22,40 +19,29 @@ public class JumpHandler {
 
 	private static final HashMap<SegmentController, Boolean> events = new HashMap<>();
 
-	public static void onJumpEngage(final ShipJumpEngageEvent event) {
+	public static void onJumpEngage(ShipJumpEngageEvent event) {
 		if(!event.getController().isOnServer()) return;
-		if(events.containsKey(event.getController())) {
-			if(events.get(event.getController())) {
-				events.remove(event.getController());
-				for(PlayerState playerState : GameServer.getServerState().getPlayerStatesByName().values()) {
-					if(getDistance(playerState.getCurrentSector(), event.getController().getSector(new Vector3i())) <= 10) PacketUtil.sendPacket(playerState, new JumpHudRemovePacket(event.getController()));
-				}
-			}
-			return;
-		}
+//		if(events.containsKey(event.getController())) {
+//			if(events.get(event.getController())) {
+//				events.remove(event.getController());
+//				for(PlayerState playerState : GameServer.getServerState().getPlayerStatesByName().values()) {
+//					if(getDistance(playerState.getCurrentSector(), event.getController().getSector(new Vector3i())) <= 10) PacketUtil.sendPacket(playerState, new JumpHudRemovePacket(event.getController()));
+//				}
+//			}
+//			return;
+//		}
 		//Get all nearby players and send a hud update packet to notify them of incoming jump
 		for(PlayerState playerState : GameServer.getServerState().getPlayerStatesByName().values()) {
 			if(getDistance(playerState.getCurrentSector(), event.getController().getSector(new Vector3i())) <= 10) PacketUtil.sendPacket(playerState, new JumpHudUpdatePacket(event.getController()));
 		}
 
-		event.setCanceled(true); //Cancel the event, so we can handle the jump ourselves
-		final int distance = getDistance(event.getOriginalSectorPos(), event.getNewSector());
-		final float multiplier = (event.getController().getConfigManager().apply(StatusEffectType.JUMP_DISTANCE, VoidElementManager.REACTOR_JUMP_DISTANCE_DEFAULT));
-		events.put(event.getController(), false);
-		event.getController().getNetworkObject().graphicsEffectModifier.add((byte) 1);
-		new Thread() {
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(3500L);
-				} catch(InterruptedException exception) {
-					exception.printStackTrace();
-				} finally {
-					events.put(event.getController(), true);
-					event.getController().engageJump((int) multiplier);
-				}
-			}
-		}.start();
+//		event.setCanceled(true); //Cancel the event, so we can handle the jump ourselves
+//		final int distance = getDistance(event.getOriginalSectorPos(), event.getNewSector());
+//		final float multiplier = (event.getController().getConfigManager().apply(StatusEffectType.JUMP_DISTANCE, VoidElementManager.REACTOR_JUMP_DISTANCE_DEFAULT));
+//		events.put(event.getController(), false);
+//		event.getController().getNetworkObject().graphicsEffectModifier.add((byte) 1);
+//		events.put(event.getController(), true);
+//		event.getController().engageJump((int) multiplier);
 	}
 
 	private static int getDistance(Vector3i originalSector, Vector3i newSector) {
